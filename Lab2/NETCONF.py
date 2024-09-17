3869f1a35cfa4bb4b9ea5407bf71daac5eb44cac
logan-chayet <loganchayet123@gmail.com>
2024-09-16 21:13:03 -0600

    Adding Lab 2

---
+++ Lab2/NETCONF.py
@@ -0,0 +1,33 @@
+from ncclient import manager
+
+# Cisco device details
+host = '10.200.0.1'  # Replace with your device IP address
+port = 830             # Default NETCONF port
+username = 'admin'
+password = 'admin'
+
+# Define the NETCONF filter for retrieving interface information
+netconf_filter = '''
+<filter>
+    <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+            <interface>
+                <name>Ethernet1</name>
+            </interface>
+    </interfaces>
+</filter>
+'''
+
+# Connect to the device
+with manager.connect(
+    host=host,
+    port=port,
+    username=username,
+    password=password,
+    hostkey_verify=False
+) as m:
+
+    # Send NETCONF request to get interface data
+    response = m.get(netconf_filter)
+
+    # Print or process the response
+    print(response.xml)

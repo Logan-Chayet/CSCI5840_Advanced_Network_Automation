7b68da11a57e6294b73cf91a39534e6ce64a8df7
Logan-Chayet <logan.chayet@colorado.edu>
2025-01-04 13:58:38 -0700

    Renamed folders

---
+++ scripts/SNMP.py
@@ -0,0 +1,57 @@
+import os
+import re
+import pyshark
+from csv import DictWriter
+
+devices = {"R1": "10.100.0.6","R2": "10.100.0.7","R3": "10.100.0.8","R4": "10.100.0.9","S1": "10.200.0.1","S2": "10.200.0.2", "S3": "10.100.0.3","S4": "10.100.0.4"}
+
+def getCPU():
+    for i in devices.items():
+        # Assigning variables to devices and OIDs
+        hostname = i[0]
+        ip = i[1]
+        CPUpkg = "iso.3.6.1.2.1.25.3.3.1.2.1"
+        #Command to get CPUpkg
+        output = os.popen(f'snmpget -v 2c -c NMAS {ip} {CPUpkg}').read()
+        # Parse integer
+        match = re.search(r'INTEGER: (\d+)', output)
+        CPU_num = match.group(1)
+        print(hostname+" CPU%: "+CPU_num)
+# tshark command to get pcap info: sudo tshark -a duration:10 -w traps.pcap -i CR_e1-1
+def getTraps():
+    file = "../monitoring/traps.pcap"
+    cap = pyshark.FileCapture(file)
+    traps = []
+    for packet in cap:
+        if 'SNMP' in packet:
+            traps.append(str(packet.snmp))
+    for i in traps:
+        print(i)
+    file1 = open("../monitoring/SNMP_traps.txt", "w")
+    file1.writelines(traps)
+    file1.close()
+
+def getSyslog():
+    file = "../monitoring/traps.pcap"
+    cap = pyshark.FileCapture(file)
+    traps = []
+    i = 1
+    for packet in cap:
+        if 'Syslog' in packet and packet.Syslog.level <= "5":
+            traps.append(str(packet.Syslog))
+            #match = re.search(r'Syslog message id:\s*(.*)', str(packet.Syslog))
+            match = re.search(r'%(.*)', str(packet.Syslog))
+            if not match:
+                continue
+            result = match.group(1)
+            syslog_dict = {'ID':i, 'Router': packet.Syslog.hostname, 'Message':result, 'Level': packet.Syslog.level}
+            field_names = ['ID', 'Router', 'Message', 'Level']
+            with open('Syslog.csv', 'w') as f_object:
+                dictwriter_object = DictWriter(f_object, field_names)
+                dictwriter_object.writerow(syslog_dict)
+                f_object.close()
+        i+=1
+
+#getCPU()
+#getTraps()
+getSyslog()
---
+++ website/website.py
@@ -0,0 +1,146 @@
+from flask import Flask,render_template,request
+import yaml
+import playbookCreation
+import os
+import csv
+import subprocess
+import troubleshooting
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/edge_config", methods=['GET','POST'])
+def edge_config():
+    config_output = None
+    if request.method == 'POST':
+        yaml_output = playbookCreation.createEdge() 
+        print(yaml_output)
+        
+        os.system("ansible-playbook ANSIBLE/site.yaml --tags edge")
+        
+        config_output = playbookCreation.sendConfig()
+
+        #return f"<pre>{config_output}</pre>"
+
+    return render_template('edge_config.html', output=config_output)
+
+@app.route("/core_config", methods=['GET','POST'])
+def core_config():
+    config_output = None
+    if request.method == 'POST':
+        yaml_output = playbookCreation.createCore()
+        print(yaml_output)
+
+        os.system("ansible-playbook ANSIBLE/site.yaml --tags core")
+
+        config_output = playbookCreation.sendConfig()
+
+        #return f"<pre>{config_output}</pre>"
+
+    return render_template('core_config.html', output=config_output)
+
+@app.route("/access_config", methods=['GET','POST'])
+def access_config():
+    config_output = None
+    if request.method == 'POST':
+        yaml_output = playbookCreation.createAccess()
+        print(yaml_output)
+
+        os.system("ansible-playbook ANSIBLE/site.yaml --tags access")
+
+        config_output = playbookCreation.sendConfig()
+
+        #return f"<pre>{config_output}</pre>"
+
+    return render_template('access_config.html', output=config_output)
+
+@app.route("/ztp", methods=['GET','POST'])
+def ztp():
+    ztp = None
+    if request.method == 'POST':
+        ztp = playbookCreation.get_ztp()
+        print(ztp)
+
+        #return f"<pre>{config_output}</pre>"
+
+    return render_template('ztp.html', output=ztp)
+
+
+@app.route("/get_golden_configs", methods=['GET','POST'])
+def get_golden_configs():
+    if request.method == 'POST':
+        configs = playbookCreation.getGoldenConfig()
+        return f"<pre>{configs}</pre>"
+
+    return render_template('get_golden_configs.html')
+
+@app.route("/health_check")
+def health_check():
+    return render_template('health_check.html')
+
+@app.route("/config")
+def config():
+    return render_template('config.html')
+
+@app.route("/neighborships", methods=['GET','POST'])
+def neighborships():
+    output = None
+    if request.method == 'POST':
+        output = playbookCreation.get_neighborships()
+
+    return render_template('neighborships.html', output=output)
+
+@app.route("/cpu", methods=['GET','POST'])
+def cpu():
+    output = None
+    if request.method == 'POST':
+        output = playbookCreation.get_cpu()
+
+    return render_template('cpu.html', output=output)
+
+@app.route("/route_table", methods=['GET','POST'])
+def route_table():
+    output = None
+    if request.method == 'POST':
+        output = playbookCreation.get_route_table()
+
+    return render_template('route_table.html', output=output)
+
+@app.route("/ip_connectivity", methods=['GET','POST'])
+def ip_connectivity():
+    output = None
+    if request.method == 'POST':
+        output = playbookCreation.get_ip_connectivity()
+
+    return render_template('ip_connectivity.html', output=output)
+
+def read_csv_data():
+    data = []
+    with open("../data/counts.csv", mode="r") as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            name = row["name"]
+            count = int(row["count"])
+            total = int(row["total"])
+            percentage = int((count / total) * 100)  # Calculate progress percentage
+            data.append({"name": name, "count": count, "total": total, "percentage": percentage})
+    return data
+
+@app.route('/code_coverage')
+def index():
+    data = read_csv_data()  # Get data from CSV
+    return render_template("code_coverage.html", data=data)
+
+@app.route('/troubleshoot', methods=['GET', 'POST'])
+def troubleshoot():
+    output = None
+    if request.method == 'POST':
+        output = ''.join(troubleshooting.fix_interface_state())
+    return render_template('troubleshoot.html', output=output)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
